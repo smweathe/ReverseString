d498ba1e268fe937397fe87227056a560d88d0a0
Stephen Weatherly <weatherly.steve@gmail.com>
2016-12-27 23:53:34 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,12 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) {
-		    String s = "";
-		  
-	      Scanner input = new Scanner(System.in);
-		  
-	      System.out.println("Please enter a string to be reverse: ");
-	      s = input.next();
-		  
-	      String reverse = new StringBuffer(s).reverse().toString();
-	      System.out.println(reverse);
-	    }
+	public static void main(String[] args) {
+		String s = "";
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter a string to be reverse: ");
+	        s = input.next();		  
+		String reverse = new StringBuffer(s).reverse().toString();
+	        System.out.println(reverse);
+	}
 }
